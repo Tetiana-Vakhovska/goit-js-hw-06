1b967f815c7a6b7da6d9bea2ee817e36e36e12c3
Tetiana-Vakhovska <benedyktanya@ukr.net>
2022-01-11 12:41:36 +0300

    Update task-02.js
    02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,10 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const elementUl = document.querySelector("#ingredients");
+const ingridientCard = ingredients.map((ingredient) => {
+  const ingridientLi = document.createElement('li');
+  ingridientLi.textContent = ingredient;
+  elementUl.classList.add("item");
+  console.log(ingridientLi);
+});
