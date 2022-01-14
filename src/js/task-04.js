e4743a64a30974a85fe7ecf2c597a6f47b48400c
Tetiana-Vakhovska <benedyktanya@ukr.net>
2022-01-14 23:49:04 +0300

    Update task-04.js
    04

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,1 +1,7 @@
-document.querySelector('[data-action="increment"]')
+
+const counterValue = 0;
+const button = document.querySelector('[data-action="increment"]');
+
+button.addEventListener("click", () => {
+  console.log("Button was clicked");
+});
