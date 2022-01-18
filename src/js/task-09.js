09a1f6412b53f920a5053d135458002400dee6c1
Tetiana-Vakhovska <benedyktanya@ukr.net>
2022-01-18 22:54:52 +0300

    Update task-09.js
    09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,8 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+function onButtonClick() {
+  const color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+  spanEl.textContent = color;
+}
