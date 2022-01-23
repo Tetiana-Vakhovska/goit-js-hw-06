febbb95c9d810f1297f9682a8a8cb848f24b9efe
Tetiana-Vakhovska <benedyktanya@ukr.net>
2022-01-24 00:52:46 +0300

    Update task-04.js
    04

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,7 +1,8 @@
 
-const counterValue = 0;
-const button = document.querySelector('[data-action="increment"]');
-
+const allBtnRef = document.querySelector('[data-action="increment"]')
+const decrementBtn = allBtnRef[0];
+const incrementBtn = allBtnRef[1];
 button.addEventListener("click", () => {
+  counter += 1
   console.log("Button was clicked");
 });
