a50638870c37f24ef8fc0e4bb2a7e576015a3eb2
Tetiana-Vakhovska <benedyktanya@ukr.net>
2022-01-24 23:43:54 +0300

    Update task-08.js
    08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,21 @@
-
+const emEl = mail.length;
+const passEl = password.length;
+if (emEl === 0) {console.log("Все поля должны быть заполнены!"); }
+else if (passEl === 0) {console.log("Все поля должны быть заполнены!"); }
+    else {
+        const form = document.querySelector(".js-register-form");
+        form.addEventListener("submit", onFormSubmit);
+        function onFormSubmit(event) {
+            event.preventPefaut();
+            const formEl = event.current.Target.elements;
+            const mail = formEl.email.value;
+            const password = formEl.password.value;
+            const subscription = formEl.subscription.value;
+            const formData = { mail, password, subscription, };
+            console.log(mail);
+            console.log(password);
+            console.log(formData);
+        }
+    }
+  
+ 
